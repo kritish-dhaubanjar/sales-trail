bc7dea8823b0a50f18f16e8f50f521584f1bfc47
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2026-02-08 20:20:26 +0545

    feat: add kot_items migration and KOTItem model

---
+++ api/app/Models/KOTItem.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class KOTItem extends Model
+{
+    use HasFactory;
+
+    protected $with = ['item'];
+    protected $fillable = ["quantity", "item_id"];
+
+    public function table(): BelongsTo
+    {
+        return $this->belongsTo(Table::class);
+    }
+
+    public function item(): BelongsTo
+    {
+        return $this->belongsTo(Item::class);
+    }
+}
