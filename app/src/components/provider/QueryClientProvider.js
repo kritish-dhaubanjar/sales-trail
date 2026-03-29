ef05a05caf6ace59d12b6147c7a0e948d19b2cba
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2026-03-30 01:58:48 +0545

    fix: query client in state

--- app/src/components/provider/QueryClientProvider.js
+++ app/src/components/provider/QueryClientProvider.js
@@ -1,9 +1,10 @@
 'use client';
+import { useState } from 'react';
 
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 const ReactQueryClientProvider = ({ children }) => {
-  const queryClient = new QueryClient();
+  const [queryClient] = useState(() => new QueryClient());
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
