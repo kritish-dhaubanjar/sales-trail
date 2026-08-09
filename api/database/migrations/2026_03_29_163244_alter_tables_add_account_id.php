60362c9f711253913e81ef0f8fe21c9627aef30f
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2026-08-09 18:38:09 +0545

    feat: add fiscal_year and invoice_id and fix account_id

--- api/database/migrations/2026_03_29_163244_alter_tables_add_account_id.php
+++ api/database/migrations/2026_03_29_163244_alter_tables_add_account_id.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('tables', function (Blueprint $table) {
-            $table->dropColumn('is_delivery');
+            $table->dropConstrainedForeignId('account_id');
         });
     }
 };
---
+++ api/database/migrations/2026_08_09_121825_alter_sales_add_columns_fiscal_year_and_invoice_no.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->string('fiscal_year', 7); // 2083/84
+            $table->integer('invoice_id');
+            $table->unique(['fiscal_year', 'invoice_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->dropUnique(['fiscal_year', 'invoice_id']);
+            $table->dropColumn('fiscal_year');
+            $table->dropColumn('invoice_id');
+        });
+    }
+};
